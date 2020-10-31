196c366783f00334fac8c245ce4f88db163a645f
alejandro-araya <alejandro.araya@ulead.ac.cr>
2020-10-30 21:59:27 -0600

    Practica1

---
+++ src/Pattern2.java
@@ -0,0 +1,12 @@
+public class Pattern2 {
+
+    public static void main (String[] args){
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5
+
+
+    }
+}
---
+++ src/TenNaturalNumbers1.java
@@ -0,0 +1,13 @@
+public class TenNaturalNumbers1 {
+
+    public static void main (String[] args){
+
+        int counter = 0;
+
+
+        while(counter <= 10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
