2cb47d975513ce019cff7640fb13442eb5cd5f8c
alejandro-araya <alejandro.araya@ulead.ac.cr>
2020-11-10 22:11:15 -0600

    10,11 and advances

---
+++ src/PrimeNumbers11.java
@@ -0,0 +1,33 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.lang.Math;
+import java.util.Scanner;
+
+
+public class PrimeNumbers11 {
+    public static void main(String[] args) {
+
+        int primeNumber = 0;
+
+        while (primeNumber < 5 || primeNumber > 300) {
+            Scanner pln = new Scanner(System.in);
+            System.out.println("Type a number between 5 and 300");
+            primeNumber = pln.nextInt();
+        }//while
+
+        double squareRootFromUsersNumbers = Math.sqrt(primeNumber);
+
+        int counter = 2;
+        while(counter <= squareRootFromUsersNumbers){
+            if (primeNumber % counter == 0) {
+                System.out.println("The number is not prime.");
+                break;
+            }//if
+            ++counter;
+            if(counter > squareRootFromUsersNumbers) {
+                System.out.println("The number is prime.");
+            }//if
+        }//while
+    }//main
+
+}//prime
